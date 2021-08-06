4081ecc3f9fdf92e19fe9bb66ba84a582e8703a4
Gustavo Bastos <bastosgm@outlook.com>
2021-08-05 22:18:45 -0300

    adding files

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            item:1
+        },
+        600:{
+            item:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
